0d175d91df45da4997d1b5a1cb80512e1db53901
Guillaume-G9 <ggonthier@live.fr>
2022-03-09 16:54:42 +0100

    sound machine

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-let getCount = document.querySelector("h1")
-let count = 0
-let input = document.querySelector("#text")
+
 
 document.onkeydown = function (e) {
-    e = e || window.event;
-    count ++
-    getCount.textContent = count
-    input.textContent += e.key
+    switch (e.key) {
+        case 'a':
+            document.querySelector("#a1").play();
+            break;
+        case 's':
+            document.querySelector("#a2").play();
+            break;
+        case 'd':
+            document.querySelector("#a3").play();
+            break;
+        case 'f':
+            document.querySelector("#a4").play();
+            break;
+        case 'g':
+            document.querySelector("#a5").play();
+            break;
+        case 'h':
+            document.querySelector("#a6").play();
+            break;
+        case 'j':
+            document.querySelector("#a7").play();
+            break;
+        case 'k':
+            document.querySelector("#a8").play();
+            break;
+        case 'l':
+            document.querySelector("#a9").play();
+            break;
+    }
 };
